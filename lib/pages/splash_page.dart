f67cbc59611720b39cec4e7cafc5fd708517da40
SACHINKUMARHACKER <sachinkum123567@gmail.com>
2019-08-11 10:17:28 +0530

    Added the yellow color

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -7,7 +7,7 @@ class SplashPage extends StatelessWidget {
       child: Text('This is a container parent of child text',
         style: TextStyle(fontSize: 26.0, fontStyle: FontStyle.italic),
     ),
-    color: Colors.red,
+    color: Colors.yellowAccent,
     );
   }
 }
